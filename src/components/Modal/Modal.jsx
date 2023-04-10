201827323fafb57c6c90188f16cbd4e7956b92d3
HaidaienkoK <gaidk1973@gmail.com>
2023-04-10 20:19:28 +0300

    fix Modal

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,50 +1,41 @@
+import { useEffect} from 'react';
 import PropTypes from 'prop-types';
-import { Component } from 'react';
 import css from './Modal.module.css';
 
-class Modal extends Component {
-  static propTypes = {
+export default function Modal({
+  onClose,
+  currentImageUrl,
+  currentImageDescription,
+}) {
+
+  useEffect(() => {
+    window.addEventListener('keydown', handleKeyDown);
+  });
+
+  const handleClickBackdrop = e => {
+    if (e.target === e.currentTarget) {
+     onClose();
+    }
+  };
+
+  const handleKeyDown = e => {
+    if (e.code === 'Escape') {
+      onClose();
+    }
+  };
+
+  return (
+    <div className={css.backdrop} onClick={handleClickBackdrop}>
+      <div className={css.modal}>
+        <img src={currentImageUrl} alt={currentImageDescription} />
+      </div>
+    </div>
+  );
+}
+
+Modal.propTypes = {
     title: PropTypes.string,
     onClose: PropTypes.func.isRequired,
     currentImageUrl: PropTypes.string,
     currentImageDescription: PropTypes.string,
   };
-
-  componentDidMount() {
-    window.addEventListener('keydown', this.handleKeyDown);
-  }
-
-  componentWillUnmount() {
-    window.removeEventListener('keydown', this.handleKeyDown);
-  }
-
-  handleClickBackdrop = e => {
-    if (e.target === e.currentTarget) {
-      this.props.onClose();
-    }
-  };
-
-  handleKeyDown = e => {
-    if (e.code === 'Escape') {
-      this.props.onClose();
-    }
-  };
-
-  render() {
-      const { currentImageUrl, currentImageDescription } =
-      this.props;
-
-    return (
-      <div className={css.backdrop} onClick={this.handleClickBackdrop}>
-        <div className={css.modal}>
-          <img
-            src={currentImageUrl}
-            alt={currentImageDescription}
-          />
-        </div>
-      </div>
-    );
-  }
-}
-
-export default Modal;
